622e896e358cf98d1027e31a7a9eb7af7faa9575
Krauserv25 <Krauserv25@MacBook-Pro-de-Ivan.local>
2013-11-07 20:34:46 +0100

    comienzos

---
+++ Activitat 1.4/js/funciones.js
@@ -0,0 +1,50 @@
+var wordsArray = new Array();
+wordsArray[0] = "PELOTA";
+wordsArray[1] = "ALIEN";
+wordsArray[2] = "JIRAFA";
+
+var auxWord = wordsArray[0]; //Palabra generada (La primera por defecto)
+
+function startGame ()
+{
+	var divGame = document.getElementById("divPlayGame");
+	divGame.style.display = 'block';
+
+	var maxWords = wordsArray.length;
+	var aleat = Math.floor(Math.random()*maxWords);
+
+	auxWord = wordsArray[aleat];
+
+	var drawWord = document.getElementById("divShowGame");
+	var innerSpan = drawWord.getElementsByTagName("span")[0];
+
+	var i;
+	var cont = 0;
+	var allLetters = "";
+
+	for (i = 0; i < wordsArray[aleat].length; i++)
+	{
+		allLetters = allLetters.concat("<input type='text' id=letter"+cont+" class='classLetters classShowLetter' value='' disabled/>");
+		cont++;
+	}
+
+	innerSpan.innerHTML = allLetters;
+}
+
+function checkLetter ()
+{
+	var letterBox = document.getElementById("divInputLetter");
+	var letter = letterBox.value;
+	letter = letter.toUpperCase();
+	var i;
+
+	for (i = 0; i < auxWord.length; i++)
+	{
+		if (auxWord[i] === letter)
+		{
+			var casilla = document.getElementById("letter"+i);
+			casilla.value = letter;
+			letterBox.value = ""; //Limpiamos la entrada de la letra
+		}
+	}
+}
