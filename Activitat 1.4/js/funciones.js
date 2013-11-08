0672a14dd563ee902b73fb823a1339a37f052374
Krauserv25 <Krauserv25@MacBook-Pro-de-Ivan.local>
2013-11-08 11:08:33 +0100

    avance1

--- Activitat 1.4/js/funciones.js
+++ Activitat 1.4/js/funciones.js
@@ -6,9 +6,16 @@ wordsArray[2] = "JIRAFA";
 var auxWord = wordsArray[0]; //Palabra generada (La primera por defecto)
 var letrasUsadas = "";
 
+var vidas = 4;
+var minutes = 0;
+var seconds = 0;
+
 function startGame ()
 {
 	var divGame = document.getElementById("divPlayGame");
+	divGame.style.display = 'block';
+
+	var divGame = document.getElementById("divTitleDisplay");
 	divGame.style.display = 'block';
 
 	var maxWords = wordsArray.length;
@@ -18,6 +25,21 @@ function startGame ()
 
 	var drawWord = document.getElementById("divShowGame");
 	var innerSpan = drawWord.getElementsByTagName("span")[0];
+
+	var letterBox = document.getElementById("divInputLetter");
+	letterBox.focus();
+
+	var chekButton = document.getElementById("buttonCheck");
+	chekButton.disabled = true;
+
+	var divCalendar = document.getElementById("divCalendarDate");
+	var innerSpanCalendar = divCalendar.getElementsByTagName("span")[0];
+
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var year = date.getFullYear();
+	innerSpanCalendar.innerHTML = day+"/"+month+"/"+year;
 
 	var i;
 	var cont = 0;
@@ -30,6 +52,8 @@ function startGame ()
 	}
 
 	innerSpan.innerHTML = allLetters;
+
+	setInterval(increaseTime, 1000);
 }
 
 function checkLetter ()
@@ -39,6 +63,7 @@ function checkLetter ()
 	letter = letter.toUpperCase();
 
 	var i;
+	var correct = false;
 
 	for (i = 0; i < auxWord.length; i++)
 	{
@@ -46,9 +71,34 @@ function checkLetter ()
 		{
 			var casilla = document.getElementById("letter"+i);
 			casilla.value = letter;
+			correct = true;
 		}
 
 		letterBox.value = ""; //Limpiamos la entrada de la letra
+		letterBox.focus();
+		var chekButton = document.getElementById("buttonCheck");
+		chekButton.disabled = true;
+	}
+
+	if (!correct)
+	{
+			vidas--;
+			var imageAhorcado = document.getElementById("imageAhorcado");
+
+			switch (vidas)
+			{
+				case 3:
+				imageAhorcado.src = "images/GIFS/Life3.gif";
+				break;
+
+				case 2:
+				imageAhorcado.src = "images/GIFS/Life2.gif";
+				break;
+
+				case 1:
+				imageAhorcado.src = "images/GIFS/Life1.gif";
+				break;
+			}
 	}
 
 	if (!lookForLetter(letter)) letrasUsadas = letrasUsadas.concat(letter+" "); 
@@ -73,4 +123,47 @@ function lookForLetter (letter)
 	}
 
 	return found;
+}
+
+function checkWrite ()
+{
+	var chekButton = document.getElementById("buttonCheck");
+
+	var letterBox = document.getElementById("divInputLetter");
+	var letter = letterBox.value;
+
+	if (letter != "")
+	{
+		chekButton.disabled = false;
+	}
+	else chekButton.disabled = true;
+}
+
+function increaseTime()
+{
+	var divTime = document.getElementById("divClockTime");
+	var innerSpan = divTime.getElementsByTagName("span")[0];
+	var auxMinutes = "";
+	var auxSeconds = "";
+
+	seconds++;
+
+	if (seconds === 60) minutes++;
+
+	seconds %= 60;
+	minutes %= 60;
+
+	if (minutes < 10) auxMinutes = "0"+minutes.toString();
+	else 
+	{
+		auxMinutes = minutes.toString();
+	}
+
+	if (seconds < 10) auxSeconds = "0"+seconds.toString();
+	else 
+	{
+		auxSeconds = seconds.toString();
+	}
+
+	innerSpan.innerHTML = auxMinutes+":"+auxSeconds;
 }
