6b9544c59c7506a826011654129966fbbde98ab7
Krauserv25 <Krauserv25@MacBook-Pro-de-Ivan.local>
2013-11-11 20:11:49 +0100

    parteJuego

--- Activitat 1.4/js/funciones.js
+++ Activitat 1.4/js/funciones.js
@@ -3,20 +3,69 @@ wordsArray[0] = "PELOTA";
 wordsArray[1] = "ALIEN";
 wordsArray[2] = "JIRAFA";
 
-var auxWord = wordsArray[0]; //Palabra generada (La primera por defecto)
-var letrasUsadas = "";
+var auxWord; //Palabra generada (La primera por defecto)
+var letrasUsadas;
+var vidas;
+var minutes;
+var seconds;
 
-var vidas = 4;
-var minutes = 0;
-var seconds = 0;
+var newWidth;
+var intervalTime;
+var intervalDivMove;
+
+function reset ()
+{
+	auxWord = wordsArray[0]; //Palabra generada (La primera por defecto)
+
+	letrasUsadas = "";
+	var divLetrasUsadas = document.getElementById("divLetrasUsadas");
+	var innerSpan = divLetrasUsadas.getElementsByTagName("span")[0];
+	innerSpan.innerHTML = letrasUsadas;
+
+	vidas = 6;
+	minutes = 0;
+	seconds = 0;
+
+	var divTime = document.getElementById("divClockTime");
+	innerSpan = divTime.getElementsByTagName("span")[0];
+	innerSpan.innerHTML = "00:00";
+
+	var divVida = document.getElementById("divVidas");
+	var innerSpanVida = divVida.getElementsByTagName("span")[0];
+
+	innerSpanVida.innerHTML = vidas;
+
+	var imageAhorcado = document.getElementById("imageAhorcado");
+	imageAhorcado.src = "images/GIFS/Life6.gif";
+				
+	newWidth = 0;
+	clearInterval(intervalTime);
+	clearInterval(intervalDivMove);
+
+	var divTiempoInvertido = document.getElementById("divTiempoInvertido");
+	var divVidasRestantes = document.getElementById("divVidasRestantes");
+	var divPuntuacion = document.getElementById("divPuntuacion");
+
+	divTiempoInvertido.style.width = newWidth+'%';
+	divVidasRestantes.style.width = newWidth+'%';
+	divPuntuacion.style.width = newWidth+'%';
+}
 
 function startGame ()
 {
-	var divGame = document.getElementById("divPlayGame");
-	divGame.style.display = 'block';
+	reset();
 
-	var divGame = document.getElementById("divTitleDisplay");
-	divGame.style.display = 'block';
+	var divMessages = document.getElementById("divMessages");
+	divMessages.style.display = 'none';
+
+	var buttonStart = document.getElementById("buttonStart");
+	buttonStart.disabled = true;
+
+	var divGame = document.getElementById("divPlayGame");
+	divGame.style.display = 'inline-block';
+
+	var divTitleGame = document.getElementById("divTitleDisplay");
+	divTitleGame.style.display = 'inline-block';
 
 	var maxWords = wordsArray.length;
 	var aleat = Math.floor(Math.random()*maxWords);
@@ -53,7 +102,8 @@ function startGame ()
 
 	innerSpan.innerHTML = allLetters;
 
-	setInterval(increaseTime, 1000);
+	intervalTime = setInterval(increaseTime, 1000);
+	intervalTime();
 }
 
 function checkLetter ()
@@ -83,10 +133,27 @@ function checkLetter ()
 	if (!correct)
 	{
 			vidas--;
+			var divVida = document.getElementById("divVidas");
+			var innerSpanVida = divVida.getElementsByTagName("span")[0];
+
+			innerSpanVida.innerHTML = vidas;
+
 			var imageAhorcado = document.getElementById("imageAhorcado");
 
 			switch (vidas)
 			{
+				case 6:
+				imageAhorcado.src = "images/GIFS/Life6.gif";
+				break;
+
+				case 5:
+				imageAhorcado.src = "images/GIFS/Life5.gif";
+				break;
+
+				case 4:
+				imageAhorcado.src = "images/GIFS/Life4.gif";
+				break;
+
 				case 3:
 				imageAhorcado.src = "images/GIFS/Life3.gif";
 				break;
@@ -99,6 +166,12 @@ function checkLetter ()
 				imageAhorcado.src = "images/GIFS/Life1.gif";
 				break;
 			}
+
+			checkLoose();
+	}
+	else
+	{
+		checkWin();
 	}
 
 	if (!lookForLetter(letter)) letrasUsadas = letrasUsadas.concat(letter+" "); 
@@ -107,6 +180,88 @@ function checkLetter ()
 	var innerSpan = divLetrasUsadas.getElementsByTagName("span")[0];
 
 	innerSpan.innerHTML = letrasUsadas;
+}
+
+function checkLoose ()
+{
+	if (vidas <= 0)
+	{
+		setMessages("FRACASO", "looseSpin", 0);
+		var buttonStart = document.getElementById("buttonStart");
+		buttonStart.disabled = false;
+
+		intervalDivMove = setInterval(divMove, 30);
+		intervalDivMove();
+	}
+}
+
+function checkWin ()
+{
+	var i = 0;
+	var all = true;
+
+	while (i < auxWord.length && all)
+	{
+		var casilla = document.getElementById("letter"+i);
+
+		if (casilla.value === "") all = false;
+
+		i++;
+	}
+
+	if (all)
+	{
+		var points = calculatePuntuation();
+
+		points = points.toFixed(0);
+
+		setMessages("ENHORABUENA", "winSpin", points);
+		var buttonStart = document.getElementById("buttonStart");
+		buttonStart.disabled = false;
+
+		intervalDivMove = setInterval(divMove, 30);
+		intervalDivMove();
+	}
+}
+
+//Se quitan puntos por vida perdida y se pierden 10 puntos por minuto
+function calculatePuntuation ()
+{
+	var points = (vidas*100)/6;
+	var resta = minutes*10;
+	
+	points -= resta;
+
+	return points;
+}
+
+function setMessages (state, image, points)
+{
+	var divGame = document.getElementById("divPlayGame");
+	divGame.style.display = 'none';
+
+	var divTitleGame = document.getElementById("divTitleDisplay");
+	divTitleGame.style.display = 'none';
+
+	var divMessages = document.getElementById("divMessages");
+	divMessages.style.display = 'inline-block';
+
+	var divStateMessage = document.getElementById("divStateMessage");
+	divStateMessage.innerHTML = "<img src='images/GIFS/"+image+".gif' class='classImageSpin' alt='looseSpin'><h2>&#161;&#161;&#161;&#161;"+state+"!!!!</h2>"+
+	"<img src='images/GIFS/"+image+".gif' class='classImageSpin' alt='looseSpin'>";
+
+	var divVidasRestantes = document.getElementById("divVidasRestantes");
+	var innerSpanVidasRestantes = divVidasRestantes.getElementsByTagName("span")[0];
+
+	var divTiempoInvertido = document.getElementById("divTiempoInvertido");
+	var innerSpanTiempoInvertido = divTiempoInvertido.getElementsByTagName("span")[0];
+
+	var divPuntuacion = document.getElementById("divPuntuacion");
+	var innerSpanPuntuacion = divPuntuacion.getElementsByTagName("span")[0];
+
+	innerSpanVidasRestantes.innerHTML = "Vidas Restantes: "+vidas;
+	innerSpanTiempoInvertido.innerHTML = "Tiempo invertido: "+getTime();
+	innerSpanPuntuacion.innerHTML = "Puntuacion total: "+points+"/100";
 }
 
 function lookForLetter (letter)
@@ -143,6 +298,13 @@ function increaseTime()
 {
 	var divTime = document.getElementById("divClockTime");
 	var innerSpan = divTime.getElementsByTagName("span")[0];
+	var auxTime = getTime();
+
+	innerSpan.innerHTML = auxTime;
+}
+
+function getTime ()
+{
 	var auxMinutes = "";
 	var auxSeconds = "";
 
@@ -165,5 +327,20 @@ function increaseTime()
 		auxSeconds = seconds.toString();
 	}
 
-	innerSpan.innerHTML = auxMinutes+":"+auxSeconds;
+	return auxMinutes+":"+auxSeconds;
+}
+
+function divMove ()
+{
+	var divTiempoInvertido = document.getElementById("divTiempoInvertido");
+	var divVidasRestantes = document.getElementById("divVidasRestantes");
+	var divPuntuacion = document.getElementById("divPuntuacion");
+
+	if (newWidth < 40)
+	{
+		newWidth++; 
+		divTiempoInvertido.style.width = newWidth+'%';
+		divVidasRestantes.style.width = newWidth+'%';
+		divPuntuacion.style.width = newWidth+'%';
+	}
 }
