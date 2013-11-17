167e9b48ff002d8c6695334a381c6eb0daaee264
Krauserv25 <Krauserv25@MacBook-Pro-de-Ivan.local>
2013-11-17 23:31:30 +0100

    LaVolta: Primer diseño (finalizado)

--- Activitat_LaVolta/js/funciones.js
+++ Activitat_LaVolta/js/funciones.js
@@ -1,5 +1,53 @@
-function init()
-{
 
+var colorSnailButtonBlack;
+var colorSnailButtonBlue;
+var colorSnailButtonRed;
+var colorSnailButtonGreen;
+var colorSnailButtonOrange;
+var colorSnailButtonPurple;
+
+var imageColor;
+
+function init ()
+{
+	colorSnailButtonBlack = document.getElementById("colorSnailButtonBlack");
+	colorSnailButtonBlack = document.getElementById("colorSnailButtonBlue");
+	colorSnailButtonBlack = document.getElementById("colorSnailButtonRed");
+	colorSnailButtonBlack = document.getElementById("colorSnailButtonGreen");
+	colorSnailButtonBlack = document.getElementById("colorSnailButtonOrange");
+	colorSnailButtonBlack = document.getElementById("colorSnailButtonPurple");
+
+	imageColor = document.getElementById("imageColor");
 }
 
+function changeSnailColor (code)
+{
+	var auxForward = "images/snail";
+
+	switch(code)
+	{
+		case 1:
+		imageColor.src = auxForward+"Black.png";
+		break;
+
+		case 2:
+		imageColor.src = auxForward+"Blue.png";
+		break;
+
+		case 3:
+		imageColor.src = auxForward+"Red.png";
+		break;
+
+		case 4:
+		imageColor.src = auxForward+"Green.png";
+		break;
+
+		case 5:
+		imageColor.src = auxForward+"Orange.png";
+		break;
+
+		case 6:
+		imageColor.src = auxForward+"Purple.png";
+		break;
+	}
+}
